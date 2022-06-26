235f5b7c52d6a8d01841f67eef2d226bda8af382
poojan411 <poojanp411@gmail.com>
2022-06-26 16:52:05 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Git hub
+        </p>
+        <div style={{ backgroundColor:'blue', padding: 2, borderRadius: 10}}>
+          <p>
+            hello world deploy on github
+          </p> 
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
